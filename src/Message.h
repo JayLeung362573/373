3e13b7d86cbed152c4f9267d46c7aade5511f8b2
Jiawei Liang <jla1028@sfu.ca>
2025-11-05 16:48:30 -0800

    update main and lobby

--- src/Message.h
+++ src/Message.h
@@ -3,13 +3,18 @@
 #include <memory>
 #include <variant>
 #include <string>
+#include <cstdint>
 #include <utility>
+#include "Lobby.h"
 
 enum class MessageType : uint8_t
 {
     Empty = 0,
     JoinGame,
     UpdateCycle,
+
+    /// Lobby messages
+    JoinLobby,
 };
 
 struct JoinGameMessage
@@ -17,18 +22,30 @@ struct JoinGameMessage
     std::string playerName;
 };
 
-struct UpdateCycleMessage{
+struct UpdateCycleMessage
+{
     int cycle;
 };
 
-using MessageData = std::variant<std::monostate, JoinGameMessage, UpdateCycleMessage>;
+struct JoinLobbyMessage{
+    std::string playerName;
+};
+
+using MessageData = std::variant<
+        std::monostate,
+        JoinGameMessage,
+        UpdateCycleMessage,
+        JoinLobbyMessage
+        >;
+
 struct Message
 {
     MessageType type = MessageType::Empty;
     MessageData data;
 };
 
-struct ClientMessage{
-    uintptr_t clientID = 0;
+struct ClientMessage
+{
+    uintptr_t clientID;
     Message message;
 };
